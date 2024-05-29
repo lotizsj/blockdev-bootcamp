637007d2a5fc32879fea8a1606ffd6a4490ad506
lotiz.sjodin@gmail.com <lotizsj@LotizLenovo.localdomain>
2024-05-29 10:30:03 +0200

    7. Token transfer delegation

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -41,16 +41,25 @@ contract Token {
 		public 
 		returns (bool success) 
 	{
-
 		require(balanceOf[msg.sender] >= _value);
-		require(_to != address(0));
 
-		balanceOf[msg.sender] = balanceOf[msg.sender] - _value;
-		balanceOf[_to] = balanceOf[_to] + _value;
-		
-		emit Transfer(msg.sender, _to, _value);
+		_transfer(msg.sender, _to, _value);
+			
 		return true;
 
+	}
+
+	function _transfer(
+		address _from,
+		address _to,
+		uint256 _value
+	) 	internal {
+		require(_to != address(0));
+
+		balanceOf[_from] = balanceOf[_from] - _value;
+		balanceOf[_to] = balanceOf[_to] + _value;
+		
+		emit Transfer(_from, _to, _value);
 	}
 
 	function approve(address _spender, uint256 _value) 
@@ -58,7 +67,6 @@ contract Token {
 	returns (bool success)
 
 	{
-		
 		require(_spender != address(0));
 		
 		allowance[msg.sender][_spender] = _value;
@@ -68,5 +76,25 @@ contract Token {
 
 	}
 	
+	function transferFrom(
+		address _from, 
+		address _to, 
+		uint256 _value
+
+	) 
+	
+	public 
+	returns (bool success)
+	
+	{
+		require(_value <= allowance[_from][msg.sender]);
+		require(_value <= balanceOf[_from]);
+
+		allowance[_from][msg.sender] = allowance[_from][msg.sender] - _value;
+
+		_transfer(_from, _to, _value);
+		
+		return true;
+	}
  
 }
